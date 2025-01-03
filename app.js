8c3993be3f5ec63d472035c89067b2f9beb703c1
jafar <jafaralihajari@gmail.com>
2025-01-03 19:23:41 +0530

    Added mongdb atlas

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Middleware
 app.use(cors()); // Enable CORS for front-end requests
-app.use(express.json()); // To parse JSON request bodies
+app.use(express.json()); // Parse JSON request bodies
 
 // Routes
 app.use('/api/properties', propertyRoutes);
@@ -16,15 +16,19 @@ app.use('/api/properties', propertyRoutes);
 // Serve static files from the "uploads" directory
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-// Database connection
-mongoose.connect('mongodb://localhost/real-estate', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((error) => console.log('MongoDB connection error:', error));
+// MongoDB Connection String
+const dbURI = "mongodb+srv://jafarhajariiam:14051999@cluster0.shrc2.mongodb.net/real-estate?retryWrites=true&w=majority";
 
-// Server setup
+// Database Connection
+mongoose
+  .connect(dbURI, { serverSelectionTimeoutMS: 5000 })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((error) => {
+    console.error('MongoDB connection error:', error);
+    process.exit(1); // Exit the process if connection fails
+  });
+
+// Server Setup
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
